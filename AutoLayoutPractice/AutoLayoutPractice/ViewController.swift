23e82508f5df06028f052e70f02c8b369c556552
yleer <spsyoudong1@naver.com>
2021-10-23 21:12:09 +0900

    10.23 autolayout practice

---
+++ AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Yundong Lee on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
